a9c5cb45aec7040d7656396ec5684541ae0305b7
stdtom <stdtom@gmx.net>
2020-05-02 15:41:47 +0200

    Fix lint issues in setup.py

--- setup.py
+++ setup.py
@@ -2,52 +2,54 @@
 
 """The setup script."""
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open("HISTORY.rst") as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = []
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    "pytest-runner",
+]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    "pytest>=3",
+]
 
 setup(
     author="StdTom",
-    author_email='stdtom@gmx.net',
-    python_requires='>=3.5',
+    author_email="stdtom@gmx.net",
+    python_requires=">=3.5",
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
-    description="SSL CertInfo collects information about SSL certificates from a set of hosts.",
-    entry_points={
-        'console_scripts': [
-            'ssl_certinfo=ssl_certinfo.cli:main',
-        ],
-    },
+    description=(
+        "SSL CertInfo collects information about SSL certificates from a set of hosts."
+    ),
+    entry_points={"console_scripts": ["ssl_certinfo=ssl_certinfo.cli:main"]},
     install_requires=requirements,
     license="Apache Software License 2.0",
-    long_description=readme + '\n\n' + history,
+    long_description=readme + "\n\n" + history,
     include_package_data=True,
-    keywords='ssl_certinfo',
-    name='ssl_certinfo',
-    packages=find_packages(include=['ssl_certinfo', 'ssl_certinfo.*']),
+    keywords="ssl_certinfo",
+    name="ssl_certinfo",
+    packages=find_packages(include=["ssl_certinfo", "ssl_certinfo.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/stdtom/ssl_certinfo',
-    version='0.1.0',
+    url="https://github.com/stdtom/ssl_certinfo",
+    version="0.1.0",
     zip_safe=False,
 )
